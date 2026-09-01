35bea10634772b6d2a11859f48bcd8dfc878eac0
Cameron Sanderson <camerontjs@gmail.com>
2026-09-01 08:30:37 -0400

    research: expose independent Contract A RC2 API

---
+++ research/contract-a-rc2-fresh-reproduction/independent/__init__.py
@@ -0,0 +1,23 @@
+from .contract_a_rc2 import (
+    ContractA,
+    ContractAValidationError,
+    canonical_handoff_bytes,
+    compute_handoff_sha256,
+    parse_json,
+    retrieval_targets,
+    sha256_text,
+    source_contract_projection,
+    validate_candidate,
+)
+
+__all__ = [
+    "ContractA",
+    "ContractAValidationError",
+    "canonical_handoff_bytes",
+    "compute_handoff_sha256",
+    "parse_json",
+    "retrieval_targets",
+    "sha256_text",
+    "source_contract_projection",
+    "validate_candidate",
+]
